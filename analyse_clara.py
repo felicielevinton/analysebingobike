20ac25b3f0af22a82502dd316c05f84588562104
felicielevinton <fe.levinton@gmail.com>
2024-09-11 07:29:33 +0000

    nouveautés

--- analyse_clara.py
+++ analyse_clara.py
@@ -1,31 +1,83 @@
-from kneed import DataGenerator, KneeLocator
-import matplotlib.pyplot as plt
-import pandas as pd
-import csv
-import os
-import scipy.io
-import json
-import numpy as np
-from format_data import *
-from utils import *
-
-t_pre = 0.2#0.2
-t_post = 0.50#0.300
-bin_width = 0.005
-# Créer les bins de temps"
-psth_bins = np.arange(-t_pre, t_post, bin_width)
-#gc = np.arange(0, 32)
-
-
-path = '/mnt/working2/felicie/data2/eTheremin/ALTAI/ALTAI_20240722_SESSION_02/'
-type = get_session_type_final(path)
-
-
-data = np.load(path+'headstage_0/data_0.005.npy', allow_pickle=True)
-features = np.load(path+'headstage_0/features_0.005.npy', allow_pickle=True)
-#gc = np.load(path+'headstage_0/good_clusters.npy', allow_pickle=True)
-gc = np.arange(0, 32)
-
-# afficher les psth par cluster
-
-# afficher la moyenne des psth de tous les clusters
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from kneed import DataGenerator, KneeLocator\n",
+    "import matplotlib.pyplot as plt\n",
+    "import pandas as pd\n",
+    "import csv\n",
+    "import os\n",
+    "import scipy.io\n",
+    "import json\n",
+    "import numpy as np\n",
+    "from format_data import *\n",
+    "from utils import *"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "t_pre = 0.2#0.2\n",
+    "t_post = 0.50#0.300\n",
+    "bin_width = 0.005\n",
+    "# Créer les bins de temps\"\n",
+    "psth_bins = np.arange(-t_pre, t_post, bin_width)\n",
+    "gc = np.arange(0, 32)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#\n",
+    "path = '/mnt/working2/felicie/data2/eTheremin/ALTAI/ALTAI_20240722_SESSION_02/'\n",
+    "type = get_session_type_final(path)\n",
+    "print(type)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "data = np.load(path+'headstage_0/data_0.005.npy', allow_pickle=True)\n",
+    "features = np.load(path+'headstage_0/features_0.005.npy', allow_pickle=True)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#tracer le psth moyen par cluster (par neurone)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#tracer le psth moyen pour tous les clusters confondus"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
---
+++ plot_psth.py
@@ -0,0 +1,52 @@
+#psth tracking vs playback par cluster et save le plot
+
+from kneed import DataGenerator, KneeLocator
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import os
+import scipy.io
+import json
+import numpy as np
+from format_data import *
+from utils import *
+import pickle
+
+t_pre = 0.2#0.2
+t_post = 0.50#0.300
+bin_width = 0.005
+# Créer les bins de temps"
+psth_bins = np.arange(-t_pre, t_post, bin_width)
+#gc = np.arange(0, 32)
+
+path = '/mnt/working2/felicie/data2/eTheremin/ALTAI/ALTAI_20240814_SESSION_00/'
+
+data = np.load(path+'headstage_0/data_0.005.npy', allow_pickle=True)
+features = np.load(path+'headstage_0/features_0.005.npy', allow_pickle=True)
+#gc = np.load(path+'headstage_0/good_clusters.npy', allow_pickle=True)
+gc = np.arange(0, 32)
+
+tail = get_psth(data, features, t_pre, t_post, bin_width, gc, 'tail')
+tracking = get_psth(data, features, t_pre, t_post, bin_width, gc, 'tracking')
+mc = get_psth(data, features, t_pre, t_post, bin_width, gc, 'mapping change')
+playback = get_psth(data, features, t_pre, t_post, bin_width, gc, 'playback') 
+
+np.save(path+'headstage_0/psth_tracking_0.005.npy', tracking)
+np.save(path+'headstage_0/psth_playback_0.005.npy', playback)
+np.save(path+'headstage_0/psth_mappingchange_0.005.npy', mc) 
+np.save(path+'headstage_0/psth_tail_0.005.npy', tail)
+
+
+# pour plot cluster par cluster16
+fig, axes = plt.subplots(4, 8, figsize=(16, 8))
+fig.suptitle('tracking vs playback', y=1.02)
+plt.subplots_adjust() 
+num_plots, num_rows, num_columns = get_better_plot_geometry(gc)
+psth_bins = np.arange(-t_pre, t_post, bin_width)
+for cluster in range(num_plots):
+    if cluster < num_plots: 
+        row, col = get_plot_coords(cluster)
+        axes[row, col].plot(psth_bins, np.nanmean(tracking[cluster], axis=0), c = 'red')
+        axes[row, col].plot(psth_bins, np.nanmean(playback[cluster], axis=0), c = 'black')
+        axes[row, col].axvline(0, c = 'grey', linestyle='--')
+        axes[row, col].set_title(f'Cluster {cluster}')
