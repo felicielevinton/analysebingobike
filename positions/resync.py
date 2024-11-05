5a5432b27eb9567cc494eb732e435ed9011902e3
flevinton <fe.levinton@gmail.com>
2024-11-05 14:39:53 +0100

    positions

--- positions/resync.py
+++ positions/resync.py
@@ -314,3 +314,34 @@ def frequency_correction(folder):
 if __name__ == "__main__":
     a, b, c, e = frequency_correction(
         "/Users/flavienferal/data/EXPERIMENT/MUROLS/MUROLS_20230218/MUROLS_20230218_SESSION_01")
+    
+
+def clean_positions(positions):
+    """
+    Fonction de nettoyage des positions enregistrées au cours de l'expérience. Pour rappel, une valeur de -1, indique que le sujet n'a pas été détecté par le réseau de neurones.
+    :param positions: 
+    :return: 
+    """
+    y = np.where(positions == -1)[0]
+    diff_y = np.diff(y)
+    diff_y = np.vstack((np.arange(1, len(diff_y) + 1), diff_y)).T
+    k = 0
+    begin = 0
+    for i, elt in diff_y:
+        if elt != 1:
+            positions[y[i - 1]] = positions[y[i - 1] + 1]
+            positions[y[i]] = positions[y[i] - 1]
+            if k != 0:
+                end = y[i - 1]
+                filler = np.full(shape=k, fill_value=positions[begin - 1])
+                positions[begin:end] = filler
+                k = 0
+        else:
+            if k == 0:
+                begin = y[i - 1]
+            k += 1
+    remainder = np.where(positions == -1)[0]
+    if len(remainder) != 0 and k != 0:
+        pass
+
+    return positions
