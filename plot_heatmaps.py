5fe57179bb766bdbc629bd97c207ae5e939816f6
flevinton <fe.levinton@gmail.com>
2024-11-07 16:51:21 +0100

    nouvelle méthode pour in/out qui marche bien

--- plot_heatmaps.py
+++ plot_heatmaps.py
@@ -5,6 +5,7 @@ from skimage import measure
 import os
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 path = "/Volumes/data2/eTheremin/ALTAI/ALTAI_20240809_SESSION_00/"
 
 #session = 'MMELOIK_20241029_SESSION_00'
@@ -18,18 +19,25 @@ path = "/Volumes/data2/eTheremin/ALTAI/ALTAI_20240809_SESSION_00/"
 
 session = '/MUROLS_20230220/MUROLS_20230220_SESSION_00'
 path = '/auto/data2/eTheremin/MUROLS/'+ session + '/'
+=======
+#path = "/auto/data6/eTheremin/OSCYPEK/OSCYPEK/OSCYPEK_20240709_SESSION_01/"
+#session = '/MMELOIK_20241107_SESSION_00'
+# path = '/auto/data6/eTheremin/MMELOIK/'+ session + '/'
+
+path = '/auto/data2/eTheremin/MUROLS/MUROLS_20230221/MUROLS_20230221_SESSION_00'
+>>>>>>> 5fb8e761 (nouvelle méthode pour in/out qui marche bien)
 
 t_pre = 0.5#0.2
 t_post = 0.50#0.300
 bin_width = 0.005
 # Créer les bins de temps"
 psth_bins = np.arange(-t_pre, t_post, bin_width)
-condition = 'playback' #or playback
+condition = 'tracking' #or playback
 
 data = np.load(path+'headstage_0/data_0.005.npy', allow_pickle=True)
 features = np.load(path+'headstage_0/features_0.005.npy', allow_pickle=True)
-gc = np.load(path+'headstage_0/good_clusters.npy', allow_pickle=True)
-#gc = np.arange(32)
+#gc = np.load(path+'headstage_0/good_clusters.npy', allow_pickle=True)
+gc = np.arange(32)
 
 tones = get_played_frequency(features, t_pre, t_post, bin_width, condition)
 tones = [int(x) for x in tones]
